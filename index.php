a5c058fb367706bf43ab3f369c775f1165d93b4e
simple-peacock <peter@simplepeacock.com>
2014-08-06 23:06:48 +1000

    Added a footer and improved some of the layout of index.php.

--- index.php
+++ index.php
@@ -24,15 +24,22 @@ require_once 'classes/DB.php';
     <link href="css/my-styles.css" rel="stylesheet" media="screen">
 </head>
 <body>
-<div class="container">
-	<h1>GLaDOS</h1>
-	<div class="row">
-		<div class="col-md-6"><!--  1st column -->
-			<p>(Genetic Lifeform and Disk Operating System) is a fictional artificially intelligent computer system and the main antagonist in the game Portal. She was created by Erik Wolpaw and Kim Swift and is voiced by Ellen McLain. She is responsible for testing and maintenance in Aperture Science research facility in both video games.</p>
-			<h3>How it works</h3>
-			<p>GLaDOS uses "Festival" - a command line speech API developed by The Centre for Speech Technology Reasearch - University of Edinburgh. The server creates a text file with the speech text which is sent to a command line utility to be converted to a wave sound file. This is then converted to a much smaller mp3 file using the "lame" command line utility which is then sent to the client's browser. The voice used is cmu_us_slt_arctic by Carnegie Mellon University.</p>
-		</div><!-- ./1st column -->
 
+<!-- main container -->
+<div class="container">
+	
+	<h1>GLaDOS</h1>
+
+	<!-- 1st row -->
+	<div class="row">
+		
+		<!--  1st column -->
+		<div class="col-md-6">
+			<p>(Genetic Lifeform and Disk Operating System) is a fictional artificially intelligent computer system and the main antagonist in the game Portal. She was created by Erik Wolpaw and Kim Swift and is voiced by Ellen McLain. She is responsible for testing and maintenance in Aperture Science research facility in both video games.</p>
+		</div>
+		<!--  ./1st column -->
+
+		<!-- 2nd column -->
 		<div class="col-md-6">
 			<div class="theform">
 				<form id="tosay" name="tosay" role="form" method="POST" action="">
@@ -51,17 +58,51 @@ require_once 'classes/DB.php';
 					<button type="submit" id="submit" class="btn btn-primary">Say It</button>
 				</form>
 			</div>
+		</div>
+		<!-- ./2nd column -->	
+	</div>
+	<!-- ./1st row -->
 
+	<!-- 2nd row -->
+	<div class="row">
+		
+		<!--  1st column -->
+		<div class="col-md-6">
+			<h3>How it works</h3>
+			<p>GLaDOS uses "Festival" - a command line speech API developed by The Centre for Speech Technology Reasearch - University of Edinburgh. The server creates a text file with the speech text which is sent to a command line utility to be converted to a wave sound file. This is then converted to a much smaller mp3 file using the "lame" command line utility which is then sent to the client's browser. The voice used is cmu_us_slt_arctic by Carnegie Mellon University.</p>
+		</div>
+		<!--  ./1st column -->
+
+		<!-- 2nd column -->
+		<div class="col-md-6">
 			<div class="messages" id="messages">
-        <?php include_once 'storedmessages.php' ?>
+        		<?php include_once 'storedmessages.php' ?>
 			</div>
-		</div><!-- ./2nd column -->
-	</div><!-- ./row	 -->
-</div><!-- ./container -->
+		</div>
+		<!-- ./2nd column -->
+	</div>
+	<!-- ./2nd row -->
+	 
+</div><!-- ./main container -->
 
+<!-- footer -->
+<div class="footer">
+    <div class="container">
+    	<ul>
+		    <li><a href="http://simplepeacock.com/projects">Back to Projects</a></li>
+		    <li><a href="http://simplepeacock.com">Home</a></li>
+		    <li><a href="https://github.com/simple-peacock/glados">GLaDOS on GitHub</a></li>
+		</ul>
+		<p class="text-muted">&copy; Simple Peacock Web Solutions 2014</p>
+    </div>
+</div>
+<!-- ./footer -->
+
+<!-- JavaScript includes -->
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="bs/js/bootstrap.min.js"></script>
 
+<!-- AJAX post with jQuery -->
 <script type="text/javascript">
 	$(document).ready(function() {
 
@@ -75,20 +116,20 @@ require_once 'classes/DB.php';
 			// check for spaces only
 			if(texttosay.length <= 255 && texttosay.length > 0) {
 
-        //ajax request to the process.php file
-        //which returns sound file
+	        //ajax request to the process.php file
+	        //which returns sound file
 				$.post('process.php', {t:texttosay},
 					function(data) {
 						$("#texttosay").append(data);
 
-            //clear the text input field
-            $('#texttosay').val('');
+			            //clear the text input field
+			            $('#texttosay').val('');
 
-            //fetch the stored messages and show updated list
-            //including message we just submitted
-            $.get('storedmessages.php', function(data) {
-              $('#messages').html(data);
-            });
+			            //fetch the stored messages and show updated list
+			            //including message we just submitted
+			            $.get('storedmessages.php', function(data) {
+			              $('#messages').html(data);
+			            });
 				});
 
 			} else {
